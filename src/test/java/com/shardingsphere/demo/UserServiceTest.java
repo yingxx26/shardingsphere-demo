15d4cdbe798a8e4d98957e73381799d6e388ffcc
yingxx26 <790230828@qq.com>
2024-04-16 16:47:49 +0800

    zl

--- src/test/java/com/shardingsphere/demo/UserServiceTest.java
+++ src/test/java/com/shardingsphere/demo/UserServiceTest.java
@@ -1,11 +1,14 @@
 package com.shardingsphere.demo;
 
+import com.shardingsphere.demo.entity.User;
 import com.shardingsphere.demo.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -16,6 +19,7 @@ public class UserServiceTest {
 	
 	@Test
 	public void testProcessUsers() throws Exception {
-		userService.processUsers();
+		List<User>  x=userService.getUsers();
+		System.out.println();
 	}
 }
